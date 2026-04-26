b079e82f7522a72990b7508c9c0a88d4964e54cb
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2026-04-26 21:40:39 +0600

    feat: add weblayout and update home page

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,17 +1,13 @@
 import Image from "next/image"
-import Header from "@/components/layouts/Header"
-import Footer from "@/components/layouts/Footer"
 import ShopByCategories from "@/components/pages/shop-by-categories"
 import FeaturedProducts from "@/components/pages/FeaturedProducts";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <div>
-      <Header />
       <Image className="w-full" src="/images/hero.jpg" alt="Hero Banner" width="1200" height="400" />
       <ShopByCategories />
       <FeaturedProducts />
-      <Footer />
     </div>
   );
 }
